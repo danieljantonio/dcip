a42ed70f4482bababf1a10e6447be26570bb0984
jedidiahaaaaa <djedidiaha@gmail.com>
2019-05-08 13:51:52 +0800

    improved the accuracy of the detected area by eroding then dilating the image

--- bg_changes.py
+++ bg_changes.py
@@ -2,11 +2,10 @@ from imutils.object_detection import non_max_suppression # functions for openCV,
 import cv2
 import numpy as np
 from imutils import resize
+from time import time, sleep
 
-
-def detect_changes(frame_curr, frame_prev=0, new_h=544, new_w=960):
-    if (type(frame_prev) == "<class 'numpy.ndarray'>"):
-        frame_prev = np.array((1,1,3))
+def background_changes(frame_curr, frame_prev, new_h=544, new_w=960):
+    fnStart = time()
     original_frame = frame_curr.copy()
     (frame_h, frame_w) = original_frame.shape[:2]
     ratio_h = frame_h / new_h
@@ -14,22 +13,25 @@ def detect_changes(frame_curr, frame_prev=0, new_h=544, new_w=960):
 
     frame1 = cv2.resize(frame_prev, (new_w, new_h))
     frame2 = cv2.resize(frame_curr, (new_w, new_h))
+
     fgbg = cv2.createBackgroundSubtractorMOG2()
 
     frames = [frame1, frame2]
 
     rects = []
     roi = []
-    
+
     fgmask = fgbg.apply(frame1)
     fgmask = fgbg.apply(frame2)
 
-    kernel = np.ones((9,9), np.uint8)
+    kernel = np.ones((5,9), np.uint8)
     dilate = cv2.dilate(fgmask, kernel, iterations=2)
     closing = cv2.morphologyEx(dilate, cv2.MORPH_CLOSE, kernel)
+    erode = cv2.erode(fgmask, kernel, iterations=5)
+    dilate = cv2.dilate(erode, kernel, iterations=2)
+    contours,_ = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    contours,_ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # print("Contours found = {}".format(len(contours)))
+    # rectsimg = original_frame.copy()
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
 
@@ -39,23 +41,24 @@ def detect_changes(frame_curr, frame_prev=0, new_h=544, new_w=960):
         end_y = int((y+h) * ratio_h)
 
         rects.append((start_x, start_y, end_x, end_y))
-    
+        # cv2.rectangle(rectsimg, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
     boxes = non_max_suppression(np.array(rects))
-
+    
+    # boxesimg = original_frame.copy()
     for (start_x, start_y, end_x, end_y) in boxes:
-        cv2.rectangle(frame1, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
-        roi.append(original_frame[start_y:end_y, start_x:end_x])
+        x = end_x-start_x
+        y = end_y-start_y
+        if ( x > 150 and y > 150):
+            roi.append(original_frame[start_y:end_y, start_x:end_x])
+        # cv2.rectangle(boxesimg, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+
+    # rectsimg = cv2.resize(rectsimg, (new_w, new_h))
+    # boxesimg = cv2.resize(boxesimg, (new_w, new_h))
+    # cv2.imshow('rects', rectsimg)
+    # cv2.imshow('boxes', boxesimg)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    fnEnd = time()
+    print('[info] bg_changes took  = {:.4f} seconds'.format(fnEnd-fnStart))
     return roi
-
-
-# img1 = cv2.imread('east/img/frame1.png')
-# img2 = cv2.imread('east/img/frame2.png')
-
-# roi = detect_changes(img2, img1)
-
-# for img in roi:
-#     cv2.imshow('img', img)
-#     cv2.waitKey(0)
-
-
-# cv2.destroyAllWindows()
