5cc07b57058ade4d69b2f3306a312271130e63d9
jedidiahaaaaa <djedidiaha@gmail.com>
2019-05-26 21:47:49 +0800

    reverted screenshot to its original condition after change

--- screenshot.py
+++ screenshot.py
@@ -1,36 +1,9 @@
 from mss import mss
 import numpy as np
-import cv2
-from time import time
 
 def grab():
-    with mss() as sct:
-        monitor = sct.monitors[1]
-        sct_img = sct.grab(monitor)
-        sct_img = np.array(sct_img)
-        return sct_img[:,:,:3]
-
-def grabAll():
     with mss() as sct:
         monitor = sct.monitors[0]
         sct_img = sct.grab(monitor)
         sct_img = np.array(sct_img)
         return sct_img[:,:,:3]
-
-timelist = []
-timelistAll = []
-for i in range(100):
-        start = time()
-        img = grab()
-        end = time()
-        # print('screenshot took {:.3f} seconds'.format(end-start))
-        timelist.append(end-start)
-for i in range(100):
-        start = time()
-        img = grabAll()
-        end = time()
-        # print('screenshot took {:.3f} seconds'.format(end-start))
-        timelistAll.append(end-start)
-
-print('the time grab() took was {:.4f} seconds'.format(np.mean(timelist)))
-print('the time grabAll() took was {:.4f} seconds'.format(np.mean(timelistAll)))
